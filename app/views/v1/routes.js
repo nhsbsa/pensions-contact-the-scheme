ef69e7deb035709264f104291af0cd6084852590
“Sushmita <sushmita.joshi@nhsbsa.nhs.uk>
2025-08-19 14:40:09 +0100

    Route-member-employer

--- app/views/v1/routes.js
+++ app/views/v1/routes.js
@@ -6,3 +6,17 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+
+// re you a member or employer?
+router.post(/previous-contract/, (req, res) => {
+
+    const contract = req.session.data['member']
+
+    if (contract == 'yes') {
+        res.redirect('membership-number')
+    } else {
+        res.redirect(employer-code')
+
+    }
+});
