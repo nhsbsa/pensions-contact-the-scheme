ba3899dad09abe5b172f3461f2a77d089c05d4a5
Sushmita Joshi <122446961+nhsbsa-sujos@users.noreply.github.com>
2026-07-27 09:49:40 +0100

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -58,10 +58,10 @@ router.use('/v3/employer', versionThree);
 router.use('/v3/third-party', versionThree);
 
 const versionThree =  require('./views/v4/routes')
-router.use('/v4', versionThree);
-router.use('/v4/member', versionThree);
-router.use('/v4/employer', versionThree);
-router.use('/v4/third-party', versionThree);
+router.use('/v4', versionFour);
+router.use('/v4/member', versionFour);
+router.use('/v4/employer', versionFour);
+router.use('/v4/third-party', versionFour);
 
 module.exports = router;
 
